d80c460d98d267961f0cbe5687a41198befd9d79
Ngoc Dang <dangtienngoc@hotmail.com>
2019-03-30 00:10:08 +0700

    build file

---
+++ build/precache-manifest.88d8fd158cc6f7ce6b8ea7431a34841b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "de78e7f02596b129ceb3",
+    "url": "/static/js/main.b3f0792c.chunk.js"
+  },
+  {
+    "revision": "c82c80ecf5e6ccf795bc",
+    "url": "/static/js/2.b6945f1f.chunk.js"
+  },
+  {
+    "revision": "de78e7f02596b129ceb3",
+    "url": "/static/css/main.8fd8cc4a.chunk.css"
+  },
+  {
+    "revision": "b64bc87ee548834986fd604d5497a838",
+    "url": "/index.html"
+  }
+];
